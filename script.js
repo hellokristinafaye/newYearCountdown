27b51a897d308a1efa9396e122a0457a028b5f29
Kristina Faye <hellokristinafaye@gmail.com>
2024-07-21 20:46:36 -0700

    triple-checked math in js

--- script.js
+++ script.js
@@ -11,12 +11,8 @@ function updateCountdown() {
     let diff = newYear - currentTime;
     
     let days = Math.floor(diff / (1000 * 60 * 60 * 24))
-    console.log(days)
-    let hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
-    console.log(hours)
-    let minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60))
-    console.log(minutes)
-
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
     let seconds = Math.floor((diff % (1000 * 60))/ 1000)
 
     console.log(seconds)
