5827f7fc1de4618b695cd8f0d5b430031fdda845
Kristina Faye <hellokristinafaye@gmail.com>
2024-07-21 20:38:31 -0700

    got innerHTMLs to work! needed to fix minutesContainer from minutes

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let daysContainer = document.getElementById('days');
 let hoursContainer = document.getElementById('hours');
-let minutes = document.getElementById('minutes');
+let minutesContainer = document.getElementById('minutes');
 let secondsContainer = document.getElementById('seconds');
 
+function updateCountdown() {
+    let newYear = new Date("January 1, 2025 00:00:00");
+    // console.log(newYear);
+    let currentTime = new Date();
+    // console.log(currentTime);
+    let diff = newYear - currentTime;
+    
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    console.log(days)
+    let hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    console.log(hours)
+    let minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60))
+    console.log(minutes)
+    let seconds = Math.floor(diff % (1000 * 60))
+    console.log(seconds)
+
+    daysContainer.innerHTML = days + "<span> Days</span>"
+    hoursContainer.innerHTML = hours + "<span> Hours</span>"
+    minutesContainer.innerHTML = minutes + "<span> Minutes</span>"
+    secondsContainer.innerHTML = seconds + "<span> Seconds</span>"
 
 
+}
+updateCountdown();
 console.log("hello there");
